2d2b9090fe3b64d92ee4c6f8df573c8c2032bb92
hollyjanedalton <hjdalton89@gmail.com>
2022-04-04 21:41:52 +0200

    Added github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This app was coded by Holly Dalton and is{" "}
+        <a
+          href="https://github.com/hollyjanedalton/react-weather"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
