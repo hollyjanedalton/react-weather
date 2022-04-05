51bcf6c7c08aa19fed7828cf4006489b07ad37aa
hollyjanedalton <hjdalton89@gmail.com>
2022-04-05 12:42:26 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This app was coded by Holly Dalton and is{" "}
-        <a
-          href="https://github.com/hollyjanedalton/react-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This app was coded by Holly Dalton and is{" "}
+          <a
+            href="https://github.com/hollyjanedalton/react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
